069120438f0f1ab324a8b8f1c516adfb61e46bd7
unknown <jhonatancastrillon34@gmail.com>
2023-08-16 14:07:15 -0500

    install bodyParser and extend limit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const cors = require("cors");
 const { config } = require("./config/config");
 const routerApi = require("./v1/routes");
@@ -7,11 +8,13 @@ const { port, host } = config;
 
 const app = express();
 
-app.use(express.json({ limit: '2mb' }));
-app.use(express.static('public'))
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 app.use(cors());
 
-routerApi(app)
+routerApi(app);
 
 app.listen(port, host, () => {
   console.log(`Server on http://${host}:${port}`);
